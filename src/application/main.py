0e5cbcd28719db621bb33ed2cd044ebf19614f95
lucascorreia <lucascorreia@bne-empregos.com.br>
2023-02-27 15:16:17 -0300

    ADD: features e testes Adicionando modelos de de classes e classe base para extrair js das urls. Adicionando pacote kivy para feature de interface (aquivos .kv teste)

--- src/application/main.py
+++ src/application/main.py
@@ -1,20 +1,25 @@
-from src.data.repositories.team_gold_repository import TeamGoldRepository
-from src.data.repositories.team_gold_repository2 import TeamGoldRepository2
-
-from src.services.team_gold_service import GoldDealer
-from src.domain.models.team_gold_model import TeamGold
+from src.domain.models.champion_base_model import ChampionBaseModel
+from src.domain.models.live_details_model import LiveDetailsModel, LiveDetailsParticipant
+from src.domain.models.live_stats_model import LiveStatusModel
 
 
 def main():
-    repo = TeamGoldRepository() #Repositório que quero usar
-    repo2 = TeamGoldRepository2() #Repositório que quero usar
-    service = GoldDealer(repo) #Serviço que usa o repositório
-    model = TeamGold() #Model a ser preenchida pelos dados
+    api_url = "https://feed.lolesports.com/livestats/v1/window/109625152897792292?hl=pt-BR&startingTime=2023-02-18T10:55:30.000Z"
 
-    model.player_1_name = service.return_player_name()
-    model.player1 = service.return_team_gold()
-    print(model)
-    print(model.player_1_name, model.get_all_players())
+    live_url = "https://feed.lolesports.com/livestats/v1/details/109539822194338807?hl=pt-BR&startingTime=2023-02-27T17:18:40.000Z"
+
+    live_status = LiveDetailsModel.instantiate(LiveDetailsModel.get_json(live_url))
+
+    participant = LiveDetailsParticipant.instantiate(live_status.frames[0]['participants'][0])
+
+    print(participant)
+
+    model = LiveStatusModel.instantiate(LiveDetailsModel.get_json(api_url))
+    names = model.get_champions_blue_team
+    chamdata = ChampionBaseModel.instantiate(ChampionBaseModel.get_json("http://ddragon.leagueoflegends.com/cdn/13.3.1/data/en_US/champion.json"))
+
+    for champ in model.get_champion_information_list(names):
+        print(f"{champ.id} tem {champ.stats.attackdamage} de atk")
 
 
 if __name__ == "__main__":
---
+++ src/domain/models/live_details_model.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass, field
+from src.data.apis.api_json_parser import DataClassUnpack
+
+@dataclass
+class LiveDetailsParticipant(DataClassUnpack):
+    participantId: str
+    level: str
+    kills: str
+    deaths: str
+    assists: str
+    totalGoldEarned: str
+    creepScore: str
+    killParticipation: str
+    championDamageShare: str
+    wardsPlaced: str
+    wardsDestroyed: str
+    attackDamage: str
+    abilityPower: str
+    criticalChance: str
+    attackSpeed: str
+    lifeSteal: str
+    armor: str
+    magicResistance: str
+    tenacity: str
+    items: list[str]
+    abilities: list[str]
+@dataclass
+class LiveDetailsModel(DataClassUnpack):
+    frames: str
+
